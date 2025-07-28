889650d8a22bc9f03d2098f7540a5b0567e16640
jcopari- <jcopari-@student.42sp.org.br>
2025-07-28 17:32:27 -0300

    Strrchr finalmente

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: jcopari- <jcopari-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 18:53:23 by jcopari-          #+#    #+#             */
-/*   Updated: 2025/07/23 19:48:14 by jcopari-         ###   ########.fr       */
+/*   Updated: 2025/07/28 17:27:29 by jcopari-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,15 @@
 
 size_t	ft_strlen(const char *str)
 {
-	size_t	i;
+	size_t		i;
 
 	i = 0;
 	while (str[i])
-	{
 		i++;
-	}
 	return (i);
 }
 
-char	ft_strcpy(char *dest, const char *src)
+char	*ft_strcpy(char *dest, const char *src)
 {
 	size_t	i;
 
@@ -48,5 +46,5 @@ char	*ft_strdup(const char *s)
 	if (dest == NULL)
 		return (NULL);
 	ft_strcpy(dest, s);
-	return ((char *)dest);
+	return (dest);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,19 +6,34 @@
 /*   By: jcopari- <jcopari-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 17:03:42 by jcopari-          #+#    #+#             */
-/*   Updated: 2025/07/25 17:12:57 by jcopari-         ###   ########.fr       */
+/*   Updated: 2025/07/28 17:32:06 by jcopari-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
 char	*strrchr(const char *s, int c)
 {
-	const char	*ptr;
-	size_t		i;
+	const char		*ptr;
+	unsigned char	chr;
 
-	ptr = *s;
-	i = (ft_strlen(s) + 1);
-
-
+	chr = (unsigned char)c;
+	ptr = s + ft_strlen(s);
+	while (ptr >= s)
+	{
+		if (*ptr == chr)
+			return ((char *)ptr);
+		ptr--;
+	}
+	return (NULL);
 }
