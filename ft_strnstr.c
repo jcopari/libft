5401348608345cc8ca5af8537077c461073b0bb7
jcopari- <jcopari-@student.42sp.org.br>
2025-08-05 18:29:51 -0300

    Arrumando o strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,13 +6,11 @@
 /*   By: jcopari- <jcopari-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 18:55:30 by jcopari-          #+#    #+#             */
-/*   Updated: 2025/08/05 16:10:24 by jcopari-         ###   ########.fr       */
+/*   Updated: 2025/08/05 18:29:08 by jcopari-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
@@ -20,13 +18,14 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	j;
 
 	i = 0;
-
 	if (*little == '\0')
 		return ((char *)big);
 	while (big[i] && i < len)
 	{
 		j = 0;
-		while (little[j] && big[i + j] && (i + j) < len && big[i + j] == little[j])
+		while (
+			little[j] && big[i + j] && (i + j) < len && big[i + j] == little[j]
+		)
 			j++;
 		if (little[j] == '\0')
 			return ((char *)&big[i]);
