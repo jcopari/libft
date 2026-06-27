3ad4e441738f63e9a994decb6e4a33daeaa6cb6e
João Copari <jcopari@gmail.com>
2026-06-27 20:20:09 -0300

    Updating the ft_lstnew

--- libft.h
+++ libft.h
@@ -6,15 +6,15 @@
 /*   By: jcopari- <jcopari-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/22 18:56:59 by jcopari-          #+#    #+#             */
-/*   Updated: 2026/06/22 19:05:06 by jcopari-         ###   ########.fr       */
+/*   Updated: 2026/06/27 16:24:16 by jcopari-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 #define LIBFT_H
 
-
-
+# include <stdlib.h>
+# include <unistd.h>
 
 //##### Linked List.
 
@@ -24,6 +24,6 @@ typedef	struct	s_list
 	struct s_list	*next; 
 }	t_list;
 
-t_list *ft_lstnew(void *content);
+t_list	*ft_lstnew(void *content);
 
 #endif 
--- libft_bonus.c
+++ libft_bonus.c
@@ -6,13 +6,28 @@
 /*   By: jcopari- <jcopari-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/22 19:06:55 by jcopari-          #+#    #+#             */
-/*   Updated: 2026/06/22 19:09:20 by jcopari-         ###   ########.fr       */
+/*   Updated: 2026/06/27 18:14:23 by jcopari-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 t_list	*ft_lstnew(void *content)
 {
-	
+    t_list *node; 
+    
+    node = malloc(sizeof(t_list));
+    if(!node)
+        return (NULL);
+    node->content = content;
+    node->next = NULL;
+    return (node);
+}
+
+int main(void)
+{
+    t_list *meu_no = ft_lstnew("texto");
+    printf("%s\n", (char *)meu_no->content);
+    return 0;
 }
